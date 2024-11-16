ed088d8b8dba2c5d0318bbfcb84f7d06abfeafd2
姜小鱼 <1847535232@qq.com>
2024-11-16 17:19:13 +0800

    fix:分页功能已写完

--- src/components/ImageAside.vue
+++ src/components/ImageAside.vue
@@ -1,16 +1,61 @@
 <template>
-    <el-aside width="220px" class="image-aside">
-      <div class="top">
-        <div v-for="i in 100" :key="i">{{ i }}</div>
-      </div>
-      <div class="bottom">
-        分页区域
-      </div>
-    </el-aside>
+  <el-aside width="220px" class="image-aside" v-loading="loading">
+
+    <div class="top">
+      <AsideList :active="activeId === item.id" v-for="(item, index) in list" :key="index">
+        {{ item.name }}
+      </AsideList>
+    </div>
+    <!-- 分页功能 -->
+    <div class="bottom">
+      <el-pagination background layout="prev,next" :total="total" :current-page="currentPage" :page-size="limit"
+        @current-change="getData" />
+    </div>
+  </el-aside>
 </template>
 
 
 <script setup>
+import { ref } from 'vue';
+import { getImageClassList } from '@/api/image_class.js';
+import AsideList from './AsideList.vue';
+
+// 加载动画
+const loading = ref(false);
+const list = ref([])
+const activeId = ref(0)
+
+// 分页
+const currentPage = ref(1)
+const total = ref(0)
+const limit = ref(10)
+
+// 获取数据
+function getData(p = null) {
+  if (typeof p === 'number') {
+    currentPage.value = p
+
+  }
+
+  loading.value = true
+  getImageClassList(currentPage.value)
+    .then(res => {
+      total.value = res.totalCount
+      list.value = res.list
+      let item = list.value[0]
+      if (item) {
+        activeId.value = item.id
+      }
+
+    })
+    .finally(() => {
+      loading.value = false
+    })
+}
+
+getData()
+
+
 
 </script>
 
